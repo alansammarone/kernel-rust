2655b31c44189e151acdd95c1c979977c1c9214d
Alan Sammarone <alansammarone@gmail.com>
2025-01-11 23:41:21 +0100

    add vga_buffer println macro and implement panic messages

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ build-bootable:
 	cargo bootimage
 
 run:
+# 	qemu-system-x86_64 -s -S -drive format=raw,file=target/target/debug/bootimage-kernel.bin
 	qemu-system-x86_64 -drive format=raw,file=target/target/debug/bootimage-kernel.bin
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,26 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-    loop {}
+    use core::fmt::Write;
+
+    println!("Hello world{}", "!");
+    // println!("Test");
+    // panic!("Some error");
+
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(
+    //     vga_buffer::WRITER.lock(),
+    //     ", some numbers: {} {}",
+    //     42,
+    //     1.337
+    // )
+    // .unwrap();
+
+    // loop {}
 }
