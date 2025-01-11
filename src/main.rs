2a65e241c717a13c3b011fb922b06bf83584d5d8
Alan Sammarone <alansammarone@gmail.com>
2025-01-12 00:28:20 +0100

    working test harness

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -12,20 +23,22 @@ fn panic(info: &PanicInfo) -> ! {
 }
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
+    // use core::fmt::Write;
+    // panic!("Some error");
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
 
     println!("Hello world{}", "!");
-    // println!("Test");
-    // panic!("Some error");
 
-    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    // write!(
-    //     vga_buffer::WRITER.lock(),
-    //     ", some numbers: {} {}",
-    //     42,
-    //     1.337
-    // )
-    // .unwrap();
+    #[cfg(test)]
+    test_main();
 
-    // loop {}
+    loop {}
+}
+
+// Tests
+#[test_case]
+fn some_assertion() {
+    print!("some_assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
