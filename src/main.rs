3ad5160236572afd8c7ad37ea71bce1b90a9894e
Alan Sammarone <alansammarone@gmail.com>
2025-01-12 01:47:13 +0100

    add testable trait

--- src/main.rs
+++ src/main.rs
@@ -46,10 +46,10 @@ enum QemuExitCode {
 }
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success);
@@ -63,10 +63,23 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 // Tests
 #[test_case]
 fn some_assertion() {
-    serial_print!("some_assertion...");
-    assert_eq!(1, 0);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
