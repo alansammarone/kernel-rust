ea9270b10ad5b5b54ba4b8df4dcbc07560c62fbf
Alan Sammarone <alansammarone@gmail.com>
2025-01-24 19:06:16 +0100

    add integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
+use kernel::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello world!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
 
 // Panic handler when not running tests
 #[cfg(not(test))]
@@ -16,73 +24,22 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
-// Panic handler when running tsts
+
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    // serial_println!("Hello world! {}", "test");
-    println!("Hello world!");
-    print!("Hello world 2!");
-    // serial_println!("Hello world!");
-
-    #[cfg(test)]
-    test_main();
-
-    loop {}
-}
-
-// QEMU + tests plumbing
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
+    kernel::test_panic_handler(info)
 }
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
     for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
+        test();
     }
 }
 
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-// Tests
 #[test_case]
-fn some_assertion() {
+fn trivial_assertion() {
     assert_eq!(1, 1);
 }
